9a6b244c456be66823751549cd1ab27d2ac35dc3
Sarah Firyal <srfiryal@gmail.com>
2023-12-21 21:01:18 +0700

    style: [SF] about background

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="page-container relative">
-    <img :src="require('@/assets/bg_hero.svg')" class="hero-bg" />
-    <HeroComponent class="hero" />
-    <AboutComponent />
+    <div class="relative">
+      <img :src="require('@/assets/bg_hero.svg')" class="hero-bg" />
+      <HeroComponent class="front" />
+    </div>
+    <div class="relative">
+      <img :src="require('@/assets/bg_about.svg')" class="about-bg" />
+      <AboutComponent class="front" />
+    </div>
   </div>
 </template>
 
@@ -20,7 +25,7 @@ export default {
 </script>
 
 <style>
-.hero {
+.front {
   position: relative;
   z-index: 1 !important;
 }
@@ -29,6 +34,18 @@ export default {
   position: absolute;
   top: 0;
   left: 0;
+  right: 0;
   width: 100%;
+  max-width: 2500px;
+  margin: 0 auto !important;
+}
+
+.about-bg {
+  position: absolute;
+  bottom: 14rem;
+  left: 0;
+  width: 42%;
+  max-width: 2500px;
+  margin: 0 auto !important;
 }
 </style>
