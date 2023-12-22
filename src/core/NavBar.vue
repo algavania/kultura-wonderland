b1b75c8298458390341c28b3207290384169b72c
Alga <algavania454@gmail.com>
2023-12-22 22:49:20 +0700

    fix: fix navbar responsive

--- src/core/NavBar.vue
+++ src/core/NavBar.vue
@@ -1,12 +1,23 @@
 <template>
   <nav class="pa-0" elevation="0" style="z-index: 100 !important">
-    <v-app-bar fixed elevation="0" height="90" color="background" class="pb-4 nav-shadow nav-height">
+    <v-app-bar
+      fixed
+      elevation="0"
+      height="90"
+      color="background"
+      class="pb-4 nav-shadow nav-height"
+    >
       <v-container>
         <div class="d-flex">
           <v-col style="position: relative">
             <div class="d-flex align-center">
               <router-link to="/" style="height: 100%">
-                <img :src="require('@/assets/logo.svg')" alt="Logo" height="70%" class="d-block mr-2" />
+                <img
+                  :src="require('@/assets/logo.svg')"
+                  alt="Logo"
+                  height="70%"
+                  class="d-block mr-2"
+                />
               </router-link>
               <router-link to="/" style="text-decoration: none">
                 <h1 class="black--text text-md">Kultura</h1>
@@ -16,12 +27,19 @@
           <v-col class="flex-grow-1 align-center d-flex justify-end">
             <v-toolbar-items class="toolbar-item">
               <div v-for="link in links" :key="link.id">
-                <div @click="navigateTo(link.id)" class="font-weight-semibold py-2 px-5 ml-3" style="cursor: pointer;">{{
-                  link.name }}</div>
+                <div
+                  @click="navigateTo(link.id)"
+                  class="font-weight-semibold py-2 px-5 ml-3"
+                  style="cursor: pointer"
+                >
+                  {{ link.name }}
+                </div>
               </div>
             </v-toolbar-items>
           </v-col>
-          <v-col class="pa-0 flex-grow-1 align-center d-flex justify-end d-lg-none">
+          <v-col
+            class="pa-0 flex-grow-1 align-center d-flex justify-end d-lg-none"
+          >
             <v-btn icon @click="drawer = !drawer">
               <v-icon>mdi-menu</v-icon>
             </v-btn>
@@ -29,15 +47,17 @@
         </div>
       </v-container>
 
-      <v-navigation-drawer v-model="drawer" app right style="background-color: #fafff2 !important" class="d-lg-none">
-        <v-list class="my-4">
-          <v-list-item v-for="link in links" :key="link.id" link>
-            <v-list-item-content>
-              <v-list-item-title @click="navigateTo(link.id)" class="text-md">{{ link.name }}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <div :class="drawer ? '' : 'd-none'" style="margin-top: 12rem; position: fixed; width: 100%;" class="background pb-3">
+        <div v-for="link in links" :key="link.id">
+          <div
+            @click="navigateTo(link.id)"
+            class="font-weight-semibold py-2 px-5 ml-3"
+            style="cursor: pointer"
+          >
+            {{ link.name }}
+          </div>
+        </div>
+      </div>
     </v-app-bar>
   </nav>
 </template>
@@ -57,10 +77,10 @@ export default {
     navigateTo(id) {
       document.getElementById(id).scrollIntoView({
         behavior: "smooth",
-        block: id == 'about' ? "center" : "start",
+        block: id == "about" ? "center" : "start",
       });
     },
-  }
+  },
 };
 </script>
 
@@ -93,3 +113,8 @@ export default {
   }
 }
 </style>
+
+<style>
+.v-toolbar__content, .v-toolbar__extension {
+    padding: 0px;
+}</style>
